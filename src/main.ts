292b03b12aad20e9af30ee504ca8cf13b3a4ef20
Samuel Cruz <samuelcruz052@gmail.com>
2023-09-14 10:47:02 -0300

    build: desactive cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   const config = new DocumentBuilder()
   .addBearerAuth()
@@ -17,6 +17,7 @@ async function bootstrap() {
   SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(new ValidationPipe())
+
   await app.listen(4000);
 }
 bootstrap();
