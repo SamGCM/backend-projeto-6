d1e3eccaa6e41ac22976128be5244fe8ca1eb4ef
Samuel Cruz <samuelcruz052@gmail.com>
2023-09-23 09:17:12 -0300

    feat: included variables env to database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,3 @@
-import { dataSourceOptions } from './../.docker/db/data-source';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -8,14 +7,31 @@ import { DashboardModule } from './dashboard/dashboard.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SchoolingModule } from './schooling/schooling.module';
 import { SkillsModule } from './skills/skills.module';
-
+import { ConfigModule } from '@nestjs/config';
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     CandidateModule, 
     RegisterModule, 
     DashboardModule, 
-    TypeOrmModule.forRoot(dataSourceOptions), SchoolingModule, SkillsModule],
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'db',
+      port: 5432,
+      username: process.env.DB_NAME,
+      password: process.env.DB_PASSWORD,
+      ssl: true,
+      url: process.env.DB_EXTERNAL_URL,
+      database: process.env.DB_NAME,
+      entities: ['dist/**/*.entity.js'],
+      migrations: ['dist/src/migrations/*.js'],
+  }), 
+  SchoolingModule, 
+  SkillsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
+
+
 export class AppModule {}
